0d0bf01a23ee0a46b037ccbac58f93a595f0c659
Sean Terry <seanterry@Seans-MBP.localdomain>
2021-10-06 12:07:18 -0700

    Imports done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect }  from "react";
 import "./App.css";
+import Axios from 'axios';
+import { NASA_URL } from './constants';
+
 
 function App() {
+  
+  // SLICE OF STATE (1)
+  const [ pod, setPod ] = useState({});
+
+  // USE EFFECT (3)
+    // API CALL
+  useEffect( () => {
+    
+  }, [])
+  
+  // REACT RENDER ... USE API DATA (2)(4)
   return (
     <div className="App">
       <p>
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const NASA_URL = 'https://api.nasa.gov/planetary/apod?api_key=Bj60eQWFPNscchx20VJ5iMdsT0tzQAboxyKyFcMW'
+// export const API_KEY = 'xyz'
